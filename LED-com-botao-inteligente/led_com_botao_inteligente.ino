062d1ec14bec441751376b63f2c1c6c19b10d1a6
andrepfdev <dreletras@gmail.com>
2023-11-24 23:12:56 -0300

    Adição de comentários

--- LED-com-botao-inteligente/led_com_botao_inteligente.ino
+++ LED-com-botao-inteligente/led_com_botao_inteligente.ino
@@ -3,7 +3,7 @@
 
 int ledRed = 13;
 int button = 4;
-int ledOn = 0; // Variável controladora
+int ledOn = 0; // variável controladora
 
 void setup()
 {  
@@ -15,18 +15,20 @@ void loop()
 {
   int valueButton = digitalRead(button);
   
+  // se o botão for pressionado a estrutura switch é chamada
   if(valueButton == HIGH){ 
-    // Se o botão for pressionado a estrutura switch é chamada
-    delay(50); // Ajuda para não ter problema com o duplo clique
-    switch(ledOn){ // Lê o valor de ledOn
-    	case 0: // No caso de ledOn for 0
+  // o delay ajuda para não ter problema com o duplo clique
+    delay(50); 
+  // switch lê o valor da ledOn
+    switch(ledOn){ 
+    	case 0: // no caso de ledOn for 0
       	digitalWrite(ledRed, HIGH);
-      	ledOn = 1; // Muda o valor da variável para 1
+      	ledOn = 1; // muda o valor da variável para 1
       	break;
-      	case 1: // No caso de ledOn for 1
+      	case 1: // no caso de ledOn for 1
       	digitalWrite(ledRed, LOW);
-      	ledOn = 0; // Muda o valor da variável para 0
-      	break; // Para o a case
+      	ledOn = 0; // muda o valor da variável para 0
+      	break; // pára a case 1
     }
   } 
   
